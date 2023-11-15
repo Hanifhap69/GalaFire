f976ad68db4ab3b24d991841160338f77de11cd6
Hanifhap69 <148093179+Hanifhap69@users.noreply.github.com>
2023-11-15 19:35:51 +0700

    HoverOverlay++

--- Assets/Script/Node Building/BuildManager.cs
+++ Assets/Script/Node Building/BuildManager.cs
@@ -18,15 +18,18 @@ public class BuildManager : MonoBehaviour
     public GameObject standardTurretPrefab;
     public GameObject missileLauncherPrefab;
 
+    public GameObject buildEffect;
+
     private TurretBlueprint turretToBuild;
 
     public bool CanBuild { get { return turretToBuild != null; } }
+    public bool HasMoney { get { return PlayerStats.Money >= turretToBuild.cost; } }
 
     public void BuildTurretOn(Node node)
     {
         if (PlayerStats.Money < turretToBuild.cost)
         {
-            Debug.Log("You Don't Have Enough Money!!");
+            Debug.Log("Not enough money to build that!");
             return;
         }
 
@@ -34,6 +37,9 @@ public class BuildManager : MonoBehaviour
 
         GameObject turret = (GameObject)Instantiate(turretToBuild.prefab, node.GetBuildPosition(), Quaternion.identity);
         node.turret = turret;
+
+        GameObject effect = (GameObject)Instantiate(buildEffect, node.GetBuildPosition(), Quaternion.identity);
+        Destroy(effect, 5f);
 
         Debug.Log("Turret build! Money left: " + PlayerStats.Money);
     }
