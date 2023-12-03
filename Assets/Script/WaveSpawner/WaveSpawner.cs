e7d972f95ffa01b7020d46c294a0180856ca8157
Hanifhap69 <148093179+Hanifhap69@users.noreply.github.com>
2023-12-04 01:41:42 +0700

    Wave_Spawner 2.0

--- Assets/Script/WaveSpawner/WaveSpawner.cs
+++ Assets/Script/WaveSpawner/WaveSpawner.cs
@@ -5,7 +5,9 @@ using UnityEngine.UI;
 public class WaveSpawner : MonoBehaviour
 {
 
-    public Transform enemyPrefab;
+    public static int EnemiesAlive = 0;
+
+    public Wave[] waves;
 
     public Transform spawnPoint;
 
@@ -18,10 +20,16 @@ public class WaveSpawner : MonoBehaviour
 
     void Update()
     {
+        if (EnemiesAlive > 0)
+        {
+            return;
+        }
+
         if (countdown <= 0f)
         {
             StartCoroutine(SpawnWave());
             countdown = timeBetweenWaves;
+            return;
         }
 
         countdown -= Time.deltaTime;
@@ -33,19 +41,29 @@ public class WaveSpawner : MonoBehaviour
 
     IEnumerator SpawnWave()
     {
-        waveIndex++;
-        
-        for (int i = 0; i < waveIndex; i++)
+        PlayerStats.Rounds++;
+
+        Wave wave = waves[waveIndex];
+
+        for (int i = 0; i < wave.count; i++)
         {
-            SpawnEnemy();
-            yield return new WaitForSeconds(0.5f);
+            SpawnEnemy(wave.enemy);
+            yield return new WaitForSeconds(1f / wave.rate);
+        }
+
+        waveIndex++;
+
+        if (waveIndex == waves.Length)
+        {
+            Debug.Log("LEVEL WON!");
+            this.enabled = false;
         }
     }
 
-    void SpawnEnemy()
+    void SpawnEnemy(GameObject enemy)
     {
-        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+        Instantiate(enemy, spawnPoint.position, spawnPoint.rotation);
+        EnemiesAlive++;
     }
 
 }
-
