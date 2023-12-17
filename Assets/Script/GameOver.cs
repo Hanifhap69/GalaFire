67d52ce95133c6341fedabfd2ccf2e31399e02fb
Hanifhap69 <148093179+Hanifhap69@users.noreply.github.com>
2023-12-18 02:58:50 +0700

    Final

--- Assets/Script/GameOver.cs
+++ Assets/Script/GameOver.cs
@@ -4,13 +4,29 @@ using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour {
 
+    public GameObject ui;
 
-	public void Retry ()
+    public void Retry ()
 	{
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 	}
 
-	public void Menu ()
+    public void Toggle()
+    {
+        ui.SetActive(!ui.activeSelf);
+
+        if (ui.activeSelf)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    
+    }
+
+    public void Menu ()
 	{
 		Debug.Log("Go to menu.");
 	}
